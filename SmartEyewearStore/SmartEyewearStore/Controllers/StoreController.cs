759aef6ad2e84ec6b85468f87b237eff02968833
arminiranpour <iranpour.armin.ca@gmail.com>
2025-07-07 03:54:31 -0400

    store deployed not complete yet

---
+++ SmartEyewearStore/SmartEyewearStore/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartEyewearStore.Data;
+using SmartEyewearStore.Models;
+
+namespace SmartEyewearStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StoreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var glasses = _context.Glasses
+                .Include(g => g.GlassesInfo)
+                .ToList();
+            return View(glasses);
+        }
+    }
+}
